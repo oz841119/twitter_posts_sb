834ce72666fb1271b26ea0a181b39a638fb41a0a
oz841119 <oz841119@gmail.com>
2022-05-31 17:13:05 +0800

    add: add fake data

---
+++ store/index.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+  postList: [
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字1231文字文字，文字文字文字文字，文字文字文字文字，文字文字23123231123123213213文字文字文字文字文字文文字文字，文字文字文字文字，文字文字文字文字，文字文字文字文字，文字文字文字文字，文字文字文字文字，文字文字文字文字，文字文字文字文字，文字文字字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15', postLink: 'https://google.com'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文文字文字文字文字文字文字文字，文，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'},
+    {userPhoto: '', userName: 'Andy Lau', postContent: 'Hello 文字文字文字文字文字文字文字文字，文字文字文字文字文字文字文字，文字文字文字文字文字文字文字', postDate: '2022/05/12 20:15'}
+  ]
+});
+
